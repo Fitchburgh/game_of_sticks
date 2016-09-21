3f69e4911caf34dc110ab8ef251a3600bc6b96fe
Brandon Fitch <fitchbj@gmail.com>
2016-09-20 21:23:50 -0400

    got the loop working. starting on the AI piece

--- run.rb
+++ run.rb
@@ -5,48 +5,40 @@ def main
   user_total_sticks = gets.chomp.to_i
   game = SticksGame.new(user_total_sticks)
   puts game
-  number_to_remove = 0
-  player1_removal = 0
-  player2_removal = 0
-  player1_all = ""
-  player2_all = ""
+  on_table_sticks = 0
+
+
 
   until game.starting_sticks <= 0
+    player1_removal = 0
+    player2_removal = 0
 
-    while !(1..3).include? player1_removal
-      puts "How many sticks would player 1 like to remove: "
-      player1_removal = gets.chomp
+      while !(1..3).include? player1_removal
+        print "How many sticks would player 1 like to remove: "
+        player1_removal = gets.chomp.to_i
 
-      player1_removal = game.sticks_to_take(player1_removal)
-
-      if !(1..3).include? player1_removal.to_i
-        puts "Can't do that sucka. Pick 1-3"
+        if !(1..3).include? player1_removal.to_i
+          puts "Can't do that sucka. Pick 1-3"
+        end
       end
-    end
-    player1_all << player1_removal
-    on_table_sticks = game.remove_sticks(player1_removal)
+      on_table_sticks = game.remove_sticks(player1_removal)
+      puts "#{on_table_sticks} sticks left on the table."
 
-    puts "Hmmm... #{on_table_sticks} left..."
-
-    if on_table_sticks <= 0
-      break
-    end
-
-    while !(1..3).include? player2_removal.to_i
-      puts "How many sticks would player 2 like to remove: "
-      player2_removal = gets.chomp
-      
-
-      player2_removal = game.sticks_to_take(player2_removal)
-
-      if !(1..3).include? player2_removal.to_i
-        puts "Can't do that sucka. Pick 1-3"
+      if on_table_sticks <= 0
+        break
       end
-    end
-    player2_all << player2_removal
-    on_table_sticks = game.remove_sticks(player2_removal)
 
-    puts "Hmmm... #{on_table_sticks} left..."
+      while !(1..3).include? player2_removal.to_i
+        print "How many sticks would player 2 like to remove: "
+        player2_removal = gets.chomp.to_i
+
+        if !(1..3).include? player2_removal.to_i
+          puts "Can't do that sucka. Pick 1-3"
+        end
+      end
+      on_table_sticks = game.remove_sticks(player2_removal)
+      puts "#{on_table_sticks} sticks left on the table."
+
 
   end
 end
--- sticks_game.rb
+++ sticks_game.rb
@@ -5,11 +5,11 @@ class SticksGame
   attr_accessor :starting_sticks
 
   def initialize(starting_sticks)
-    @takeaway_options = {
-      "1" => 1,
-      "2" => 2,
-      "3" => 3
-    }
+    # @takeaway_options = {
+    #   "1" => 1,
+    #   "2" => 2,
+    #   "3" => 3
+    # }
     @starting_sticks = starting_sticks
 
     if !(10..100).include? starting_sticks
@@ -17,18 +17,18 @@ class SticksGame
     end
   end
 
-  def sticks_to_take(user_choice)
-    @user_choice = user_choice
-    if @takeaway_options.has_key?(@user_choice)
-      number_to_remove = @takeaway_options[@user_choice]
-      return number_to_remove
+  # def sticks_to_take(user_choice)
+  #   @user_choice = user_choice
+  #   if @takeaway_options.has_key?(@user_choice)
+  #     number_to_remove = @takeaway_options[@user_choice]
+  #     return number_to_remove
     # else
     #   # !@takeaway_options.include?(@user_choice)
     #   # puts "Please choose between 1 and 3 sticks."
     #   # return
-
-    end
-  end
+  #
+  #   end
+  # end
 
   def remove_sticks(number_to_remove)
     number_to_remove = number_to_remove.to_i
